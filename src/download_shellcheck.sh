d3bfa96e4243d39cf3a0017b4b63a0af146e3db5
mendess <pedro.mendes.26@gmail.com>
2021-03-08 20:04:02 +0000

    Fix download shellcheck command

--- src/download_shellcheck.sh
+++ src/download_shellcheck.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 # Update shellcheck to the locked v0.7.0 version through the binary distribution
-scversion='v0.7.0'
-          
-wget -qO- "https://storage.googleapis.com/shellcheck/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+scversion='v0.7.1'
+
+wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 sudo cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 shellcheck --version
