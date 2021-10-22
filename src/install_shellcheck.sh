a556b1a0d977e41898fb99d58cd0b80c102dcc0c
Azbagheri <azadeh.bagheri7@gmail.com>
2021-10-21 21:39:05 -0400

    Fixed the Docker image build issue by removing sudo from the Shellcheck installation script

--- src/install_shellcheck.sh
+++ src/install_shellcheck.sh
@@ -4,5 +4,5 @@
 scversion='v0.7.2'
           
 wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
-sudo cp "shellcheck-${scversion}/shellcheck" /usr/bin/
+cp "shellcheck-${scversion}/shellcheck" /usr/bin/
 shellcheck --version
