e300210a43636b477a8cd7c7a358bd7c93be1e7a
Azbagheri <azadeh.bagheri7@gmail.com>
2021-10-21 20:27:30 -0400

    Renamed the script used to install shellcheck and bumped the shellcheck version. Also upgraded the base image used in the Shell Linter's Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM alpine:3.10.3
+FROM alpine:3.13.6
 
-RUN echo "http://dl-cdn.alpinelinux.org/alpine/v3.11/community" >> /etc/apk/repositories; \
-    apk update && apk add --no-cache bash shellcheck=0.7.0-r1
+RUN ./src/install_shellcheck
 
-RUN bash --version && shellcheck --version
+RUN bash --version 
 
 COPY entrypoint.sh /entrypoint.sh
 
--- src/install_shellcheck.sh
+++ src/install_shellcheck.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Update shellcheck to the locked v0.7.0 version through the binary distribution
-scversion='v0.7.0'
+scversion='v0.7.2'
           
 wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 sudo cp "shellcheck-${scversion}/shellcheck" /usr/bin/
