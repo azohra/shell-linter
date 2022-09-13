ef54ea41c922010f888973acf76e7b8f481ab00d
Daniel Thamdrup <dallemon@protonmail.com>
2022-09-13 16:39:48 +0200

    update alpine and shellcheck
    Signed-off-by: Daniel Thamdrup <dallemon@protonmail.com>

--- src/install_shellcheck.sh
+++ src/install_shellcheck.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-scversion='v0.7.2'
+scversion='v0.8.0'
           
 wget -qO- "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
 cp "shellcheck-${scversion}/shellcheck" /usr/local/bin
